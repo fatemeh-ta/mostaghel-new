019a5d4b9a8e947de4d2aa6f10b14526818fc443
fatemeh.ta <fatemeh.tashakkori@live.com>
2020-06-13 16:41:06 +0430

    step 1

--- js/main.js
+++ js/main.js
@@ -3,10 +3,78 @@ $(document).ready(function(){
     $(".menu-icon").click(function(){
       $(".menu-list").slideToggle();
     });
-
-
+/*------------------START SUBMENU-------------------------------- */
+    if (window.matchMedia('(max-width: 900px)').matches) {
+      $(".political-submenu").click(function(){
+        $(".sub-menu-political").slideToggle("slow");
+      });
+    
+      $(".social-submenu").click(function(){
+        $(".sub-menu-social").slideToggle("slow");
+      });
+    
+      $(".economic-submenu").click(function(){
+        $(".sub-menu-economic").slideToggle("slow");
+      });
+    
+      $(".sport-submenu").click(function(){
+        $(".sub-menu-sport").slideToggle("slow");
+      });
+    
+      $(".culture-submenu").click(function(){
+        $(".sub-menu-culture").slideToggle("slow");
+      });
+    
+      $(".mutinews-submenu").click(function(){
+        $(".sub-menu-mutinews").slideToggle("slow");
+      });
+    }
+/*------------------END SUBMENU-------------------------------- */
     if ($('#slide-pic').length> 0) {
         $("#slide-pic").lightSlider({
+            item: 1,
+            slideMove: 1, // slidemove will be 1 if loop is true
+            slideMargin: 10,
+            useCSS: true,
+            speed: 400, //ms'
+            auto: false,
+            loop: false,
+            pause: 2000,
+            keyPress: false,
+            controls: true,
+            prevHtml: '',
+            nextHtml: '',
+            rtl:false,
+            pager: true,
+            enableTouch:true,
+            enableDrag:true,
+        });
+    };
+
+    if ($('#photo-slide').length> 0) {
+        $("#photo-slide").lightSlider({
+            item: 1,
+            slideMove: 1, // slidemove will be 1 if loop is true
+            slideMargin: 10,
+            useCSS: true,
+            speed: 400, //ms'
+            auto: false,
+            loop: false,
+            pause: 2000,
+            keyPress: false,
+            controls: true,
+            prevHtml: '',
+            nextHtml: '',
+            rtl:false,
+            pager: true,
+            enableTouch:true,
+            enableDrag:true,
+        });
+    };
+
+    if ($('#slide-video').length> 0) {
+
+        $("#slide-video").lightSlider({
             item: 1,
             slideMove: 1, // slidemove will be 1 if loop is true
             slideMargin: 10,
